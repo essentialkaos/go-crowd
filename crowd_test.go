edc30e112ed3928ff763915d807d4c8f86de7d99
Anton Novojilov <andy@essentialkaos.com>
2018-08-16 13:32:36 +0300

    More tests

--- crowd_test.go
+++ crowd_test.go
@@ -31,3 +31,20 @@ func (s *CrowdSuite) TestBasic(c *C) {
 	c.Assert(api, NotNil)
 	c.Assert(err, IsNil)
 }
+
+func (s *CrowdSuite) TestAttributesHelpers(c *C) {
+	attrs := Attributes{
+		&Attribute{"test", []string{"AB", "CD"}},
+		&Attribute{"magic", []string{"ABCD"}},
+	}
+
+	c.Assert(attrs.Has("unknown"), Equals, false)
+	c.Assert(attrs.Has("test"), Equals, true)
+
+	c.Assert(attrs.GetList("unknown"), HasLen, 0)
+	c.Assert(attrs.GetList("test"), HasLen, 2)
+
+	c.Assert(attrs.Get("unknown"), Equals, "")
+	c.Assert(attrs.Get("test"), Equals, "AB CD")
+	c.Assert(attrs.Get("magic"), Equals, "ABCD")
+}
